06556948e7fd50bc305260d3f16c24b7e7f12f82
rakibhossain431 <rakibjp431@gmail.com>
2022-02-07 02:25:02 +0600

    style change

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ let list = document.querySelector('.list')
             isCompleted: false,
         }
         taskList.push(task);
+        localStoragData();
         
 
         //  e.target.insert.value = "";
@@ -30,6 +31,8 @@ let list = document.querySelector('.list')
             return `<li>
             <input type="checkbox"/>
                 <span>${item.task}</span>
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                 <button>&times;</button>
             </li>`
         }).join("")
@@ -41,6 +44,8 @@ let list = document.querySelector('.list')
 
          
     }
- 
+        function localStoragData(){
+
+        }
 
     SubmitButton.addEventListener('submit', hendlSubmit);
